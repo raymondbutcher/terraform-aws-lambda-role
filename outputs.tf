7eb1b31a2c780eea7933ff9a9c20a64c907423ec
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-18 11:51:12 +0100

    Add log group name outputs

--- outputs.tf
+++ outputs.tf
@@ -53,6 +53,28 @@ output "id" {
   ]
 }
 
+output "log_group_name" {
+  value = var.enabled ? local.log_group_name : null
+  depends_on = [
+    aws_iam_role_policy.cloudwatch_logs,
+    aws_iam_role_policy.custom_policies,
+    aws_iam_role_policy.dead_letter_config,
+    aws_iam_role_policy.policy_arns,
+    aws_iam_role_policy.vpc_config,
+  ]
+}
+
+output "log_group_name_edge" {
+  value = var.enabled ? local.log_group_name_edge : null
+  depends_on = [
+    aws_iam_role_policy.cloudwatch_logs,
+    aws_iam_role_policy.custom_policies,
+    aws_iam_role_policy.dead_letter_config,
+    aws_iam_role_policy.policy_arns,
+    aws_iam_role_policy.vpc_config,
+  ]
+}
+
 output "name" {
   value = var.enabled ? aws_iam_role.lambda[0].name : null
   depends_on = [
