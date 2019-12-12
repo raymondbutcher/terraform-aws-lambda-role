a4e830c16d1f35adeefea2f03ce6cbab24401288
Raymond Butcher <ray.butcher@claranet.uk>
2019-12-12 20:11:21 +0000

    Fix name conflict with multiple custom policies

--- policy_custom_policies.tf
+++ policy_custom_policies.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_iam_role_policy" "custom_policies" {
   count = var.enabled ? local.custom_policies_count : 0
 
-  name   = "custom_policy"
+  name   = "custom_policy${count.index > 0 ? "_${count.index + 1}" : ""}"
   role   = aws_iam_role.lambda[0].id
   policy = var.custom_policies[count.index]
 }
